f257a2285d2f88a0f54e6f05689cd0dbc001adca
Harsimran <harsimransinghbarki@gmail.com>
2021-01-09 18:44:48 +0530

    cleaned up code removed firebase auth setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
 import "../styles/globals.css";
 import { ChakraProvider, theme } from "@chakra-ui/react";
-import { ProvideAuth } from "../utils/auth";
 
 function App({ Component, pageProps }) {
   return (
-    <ProvideAuth>
-      <ChakraProvider resetCSS={true} colorModeManager theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </ProvideAuth>
+    <ChakraProvider resetCSS={true} colorModeManager theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
   );
 }
 
