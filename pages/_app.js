722b7df21caa874946cdac4a5a18f8e94b3df8e3
Harsimran <harsimransinghbarki@gmail.com>
2021-01-09 16:49:44 +0530

    added query-string isomorphic-unfetch utils/firebase-auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 import "../styles/globals.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, theme } from "@chakra-ui/react";
 
 function App({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider resetCSS={true} colorModeManager theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
