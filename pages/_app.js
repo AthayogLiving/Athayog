2258231a48a88861f648f9666c7a8c02bd7dfd82
Harsimran <harsimransinghbarki@gmail.com>
2021-01-09 18:00:30 +0530

    added react-hook-form and firebase authentication context

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import "../styles/globals.css";
 import { ChakraProvider, theme } from "@chakra-ui/react";
+import { ProvideAuth } from "../utils/auth";
 
 function App({ Component, pageProps }) {
   return (
-    <ChakraProvider resetCSS={true} colorModeManager theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ProvideAuth>
+      <ChakraProvider resetCSS={true} colorModeManager theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ProvideAuth>
   );
 }
 
