a670245b4558385587615299f193a00b70e1dd01
Harsimran <harsimransinghbarki@gmail.com>
2021-01-10 22:04:19 +0530

    added react-hook-form for login

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,43 +1,46 @@
 import React, { useState } from 'react';
-import { auth } from 'firebase';
-import Router from 'next/router';
 import { useAuth } from '@/lib/auth';
+import { useForm } from 'react-hook-form';
 import {
      Input,
-     Stack,
      Button,
-     ButtonGroup,
      Flex,
      Box,
      FormControl,
      Heading,
      FormLabel,
-     Spacer,
      Image,
      InputGroup,
+     useToast,
      InputLeftElement,
-     Text
+     Text,
+     FormErrorMessage
 } from '@chakra-ui/react';
-import {
-     PhoneIcon,
-     AddIcon,
-     WarningIcon,
-     AtSignIcon,
-     UnlockIcon
-} from '@chakra-ui/icons';
+import { AtSignIcon, UnlockIcon } from '@chakra-ui/icons';
 import Head from 'next/head';
 import BrandLogo from '../../public/logo.png';
 
 const index = () => {
      const auth = useAuth();
-     const [email, setEmail] = useState();
-     const [password, setPassword] = useState();
-     const [isLoading, setIsLoading] = useState();
-     const handleClick = (e) => {
-          e.preventDefault();
-          setIsLoading(true);
-          auth.signinWithEmail(email, password, '/admin/dashboard');
+     const toast = useToast();
+     const [loading, setLoading] = useState(false);
+     const { signinWithEmail } = useAuth();
+     const { handleSubmit, register, errors } = useForm();
+
+     const onLogin = ({ email, pass }) => {
+          setLoading(true);
+          signinWithEmail(email, pass).catch((error) => {
+               setLoading(false);
+               toast({
+                    title: 'An error occurred.',
+                    description: error.message,
+                    status: 'error',
+                    duration: 5000,
+                    isClosable: true
+               });
+          });
      };
+
      return (
           <>
                <Box bg="gray.50">
@@ -61,71 +64,122 @@ const index = () => {
                          </Text>
                          <Box
                               p={10}
-                              shadow="md"
+                              shadow={[null, 'md']}
                               bg="white"
                               borderRadius="lg"
                               w={[300, 400, 500]}
                               mt="5"
+                              as="form"
+                              onSubmit={handleSubmit((data) => onLogin(data))}
+                              register={register}
                          >
                               {!auth.user ? (
-                                   <FormControl id="admin_login">
-                                        <FormLabel>Email address</FormLabel>
-                                        <InputGroup>
-                                             <InputLeftElement
-                                                  pointerEvents="none"
-                                                  children={
-                                                       <AtSignIcon color="gray.300" />
-                                                  }
-                                             />
-                                             <Input
-                                                  variant="outline"
-                                                  type="text"
-                                                  bg="white"
-                                                  shadow="sm"
-                                                  onChange={(e) =>
-                                                       setEmail(e.target.value)
-                                                  }
-                                             />
-                                        </InputGroup>
-                                        <FormLabel mt="8">Password</FormLabel>
-                                        <InputGroup>
-                                             {' '}
-                                             <InputLeftElement
-                                                  pointerEvents="none"
-                                                  children={
-                                                       <UnlockIcon color="gray.300" />
-                                                  }
-                                             />
-                                             <Input
-                                                  variant="outline"
-                                                  type="password"
-                                                  bg="white"
-                                                  shadow="sm"
-                                                  onChange={(e) =>
-                                                       setPassword(
-                                                            e.target.value
-                                                       )
-                                                  }
-                                             />
-                                        </InputGroup>
-                                   </FormControl>
+                                   <>
+                                        <FormControl
+                                             id="admin_login"
+                                             isRequired={true}
+                                             isInvalid={
+                                                  errors.email &&
+                                                  errors.email.message
+                                             }
+                                        >
+                                             <FormLabel>
+                                                  Email address
+                                             </FormLabel>
+                                             <InputGroup>
+                                                  <InputLeftElement
+                                                       pointerEvents="none"
+                                                       children={
+                                                            <AtSignIcon color="gray.300" />
+                                                       }
+                                                  />
+                                                  <Input
+                                                       variant="outline"
+                                                       type="text"
+                                                       bg="white"
+                                                       shadow="sm"
+                                                       autoFocus
+                                                       aria-label="Email Address"
+                                                       id="email"
+                                                       name="email"
+                                                       ref={register({
+                                                            required:
+                                                                 'Please enter your email.'
+                                                       })}
+                                                       placeholder="name@athayog.com"
+                                                  />
+                                                  <FormErrorMessage>
+                                                       {errors.email &&
+                                                            errors.email
+                                                                 .message}
+                                                  </FormErrorMessage>
+                                             </InputGroup>
+                                        </FormControl>
+                                        <FormControl
+                                             isRequired={true}
+                                             isInvalid={
+                                                  errors.pass &&
+                                                  errors.pass.message
+                                             }
+                                        >
+                                             <FormLabel mt="8">
+                                                  Password
+                                             </FormLabel>
+                                             <InputGroup>
+                                                  <InputLeftElement
+                                                       pointerEvents="none"
+                                                       children={
+                                                            <UnlockIcon color="gray.300" />
+                                                       }
+                                                  />
+                                                  <Input
+                                                       variant="outline"
+                                                       type="password"
+                                                       bg="white"
+                                                       shadow="sm"
+                                                       aria-label="Password"
+                                                       name="pass"
+                                                       id="password"
+                                                       type="password"
+                                                       ref={register({
+                                                            required:
+                                                                 'Please enter a password.'
+                                                       })}
+                                                  />
+                                                  <FormErrorMessage>
+                                                       {errors.pass &&
+                                                            errors.pass.message}
+                                                  </FormErrorMessage>
+                                             </InputGroup>
+                                        </FormControl>
+                                        <Box mt={10}>
+                                             <Button
+                                                  sizs="sm"
+                                                  type="button"
+                                                  width="full"
+                                                  shadow="md"
+                                                  type="submit"
+                                                  loadingText="Submitting"
+                                                  background="purple.600"
+                                                  _hover={{
+                                                       bg: 'purple.500'
+                                                  }}
+                                                  color="white"
+                                                  isLoading={loading}
+                                                  fontWeight="medium"
+                                                  mt={4}
+                                                  h="50px"
+                                                  fontSize="lg"
+                                                  _active={{
+                                                       bg: 'gray.800',
+                                                       transform: 'scale(0.95)'
+                                                  }}
+                                             >
+                                                  Sign in
+                                             </Button>
+                                        </Box>
+                                   </>
                               ) : null}
-                              <Box mt={10}>
-                                   <Button
-                                        sizs="sm"
-                                        type="button"
-                                        width="full"
-                                        shadow="md"
-                                        isLoading={isLoading ? true : false}
-                                        loadingText="Submitting"
-                                        background="purple.600"
-                                        _hover={{ bg: 'purple.500' }}
-                                        color="white"
-                                        onClick={(e) => handleClick(e)}
-                                   >
-                                        Sign in
-                                   </Button>
-                              </Box>
                          </Box>
                     </Flex>
                </Box>
