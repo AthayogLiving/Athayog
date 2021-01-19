3ca7fd9d755f724354e5b2d063355f7924f72448
Harsimran <harsimransinghbarki@gmail.com>
2021-01-19 19:41:58 +0530

    changed offerings to const function

--- components/home/Offerings.js
+++ components/home/Offerings.js
@@ -1,12 +1,12 @@
 import { Box } from '@chakra-ui/react';
 import React from 'react';
 
-function Offerings() {
+const Offerings = () => {
      return (
           <Box height="100vh">
                <h1>Offerings</h1>
           </Box>
      );
-}
+};
 
 export default Offerings;
