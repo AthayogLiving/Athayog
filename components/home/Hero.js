e15aa45c9078dfe2843d25c101b2af5db31e8acb
Harsimran <harsimransinghbarki@gmail.com>
2021-01-19 18:05:01 +0530

    added dots for hero slider

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -1,6 +1,7 @@
-import { Box, Flex, Image } from '@chakra-ui/react';
+import { Box, Flex, Image, forwardRef, Text, HStack } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
+import { motion, isValidMotionProp, AnimatePresence } from 'framer-motion';
 
 const sliderImages = [
      {
@@ -28,10 +29,22 @@ function Hero() {
      const nextSlide = () => {
           setCurrent(current === length - 1 ? 0 : current + 1);
      };
-     console.log(current);
+
      const previousSlide = () => {
           setCurrent(current === 0 ? length - 1 : current - 1);
      };
+
+     const MotionBox = motion.custom(
+          forwardRef((props, ref) => {
+               const chakraProps = Object.fromEntries(
+                    // do not pass framer props to DOM element
+                    Object.entries(props).filter(
+                         ([key]) => !isValidMotionProp(key)
+                    )
+               );
+               return <Box ref={ref} {...chakraProps} />;
+          })
+     );
 
      return (
           <Flex
@@ -46,24 +59,50 @@ function Hero() {
                     top="50%"
                     left="10px"
                     color="black"
+                    zIndex={2}
                >
                     <IoIosArrowBack size="3rem" cursor="pointer" />
                </Box>
 
                {sliderImages.map((slide, index) => {
                     return (
-                         index === current && (
-                              <Box
-                                   bgImage={`url(${slide.image})`}
-                                   backgroundPosition="center"
-                                   backgroundRepeat="no-repeat"
-                                   height="100vh"
-                                   width="100%"
-                                   key={index}
-                              ></Box>
-                         )
+                         <>
+                              {index === current && (
+                                   <AnimatePresence key={index}>
+                                        <MotionBox
+                                             initial={{ opacity: 0, y: 0 }}
+                                             animate={{ opacity: 1 }}
+                                             exit={{ opacity: 0 }}
+                                             transition={{ duration: 1 }}
+                                             exit={{ opacity: 0 }}
+                                             bgImage={`url(${slide.image})`}
+                                             backgroundPosition="center"
+                                             backgroundRepeat="no-repeat"
+                                             height="100vh"
+                                             width="100%"
+                                        ></MotionBox>
+                                   </AnimatePresence>
+                              )}
+                         </>
                     );
                })}
+               <HStack position="absolute" bottom={5} zIndex={2}>
+                    {sliderImages.map((slide, index) => {
+                         return (
+                              <Box
+                                   height={4}
+                                   width={4}
+                                   background={
+                                        index === current
+                                             ? 'primaryGreen'
+                                             : 'white'
+                                   }
+                                   rounded="full"
+                              ></Box>
+                         );
+                    })}
+               </HStack>
+
                <Box
                     onClick={nextSlide}
                     size={10}
@@ -71,6 +110,7 @@ function Hero() {
                     top="50%"
                     right="10px"
                     color="black"
+                    zIndex={2}
                >
                     <IoIosArrowForward size="3rem" cursor="pointer" />
                </Box>
