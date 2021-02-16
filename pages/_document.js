b0c15ef306e7719a12314f59fed036835de0ff92
Harsimran <harsimransinghbarki@gmail.com>
2021-02-16 16:31:35 +0530

    installed razorpay and crypto

--- pages/_document.js
+++ pages/_document.js
@@ -3,29 +3,30 @@ import { ColorModeScript } from '@chakra-ui/react';
 import theme from '@/styles/theme';
 
 class RootDocument extends Document {
-    static async getInitialProps(ctx) {
-        const initialProps = await Document.getInitialProps(ctx);
-        return { ...initialProps };
-    }
+     static async getInitialProps(ctx) {
+          const initialProps = await Document.getInitialProps(ctx);
+          return { ...initialProps };
+     }
 
-    render() {
-        return (
-            <Html>
-                <link
-                    rel="stylesheet"
-                    href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;300;400;500;600;700&display=swap"
-                />
-                <Head />
-                <body>
-                    <ColorModeScript
-                        initialColorMode={theme.config.initialColorMode}
+     render() {
+          return (
+               <Html>
+                    <link
+                         rel="stylesheet"
+                         href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;300;400;500;600;700&display=swap"
                     />
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
+                    <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
+                    <Head />
+                    <body>
+                         <ColorModeScript
+                              initialColorMode={theme.config.initialColorMode}
+                         />
+                         <Main />
+                         <NextScript />
+                    </body>
+               </Html>
+          );
+     }
 }
 
 export default RootDocument;
