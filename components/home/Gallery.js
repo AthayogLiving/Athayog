55b9222fa48866c0895c8a551e2f493ee781c5e3
Harsimran <harsimransinghbarki@gmail.com>
2021-02-19 00:27:07 +0530

    cleaned up gallery

--- components/home/Gallery.js
+++ components/home/Gallery.js
@@ -1,17 +1,10 @@
 import fetcher from '@/utils/fetcher';
 import { Box, Heading, Skeleton } from '@chakra-ui/react';
 import React from 'react';
-
 import useSWR from 'swr';
-
 import Image from 'next/image';
-// Import Swiper React components
 import Swiper from 'react-id-swiper';
-
 import SwiperCore, { Navigation, Pagination, EffectFade } from 'swiper';
-
-// Import Swiper styles
-
 import 'swiper/swiper-bundle.css';
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 
