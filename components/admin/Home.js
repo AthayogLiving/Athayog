6d0867e5c4e3019d4b6cb01885cf460d12fc3239
Harsimran <harsimransinghbarki@gmail.com>
2021-02-20 22:21:40 +0530

    cleaned dashboard home and removed ununsed api and routes

--- components/admin/Home.js
+++ components/admin/Home.js
@@ -1,38 +1,29 @@
 import React from 'react';
 import useSWR from 'swr';
 import fetcher from '@/utils/fetcher';
-import {
-     Box,
-     Flex,
-     Grid,
-     Heading,
-     Skeleton,
-     Spinner,
-     Stack,
-     Text
-} from '@chakra-ui/react';
-import { FiFileText, FiUsers } from 'react-icons/fi';
+import { Grid, Spinner, Stack } from '@chakra-ui/react';
+
 import { useAuth } from '@/lib/auth';
 
 function Home() {
-     const { user } = useAuth();
-     const { data } = useSWR(
-          user ? [`/api/user/details`, user.token] : null,
-          fetcher
-     );
-     if (!data) {
-          return (
-               <Grid placeItems="center" mt={10}>
-                    <Spinner
-                         thickness="4px"
-                         speed="0.65s"
-                         emptyColor="gray.200"
-                         color="blue.500"
-                         size="xl"
-                    />
-               </Grid>
-          );
-     }
+     // const { user } = useAuth();
+     // const { data } = useSWR(
+     //      user ? [`/api/user/details`, user.token] : null,
+     //      fetcher
+     // );
+     // if (!data) {
+     //      return (
+     //           <Grid placeItems="center" mt={10}>
+     //                <Spinner
+     //                     thickness="4px"
+     //                     speed="0.65s"
+     //                     emptyColor="gray.200"
+     //                     color="blue.500"
+     //                     size="xl"
+     //                />
+     //           </Grid>
+     //      );
+     // }
      return <Stack d="flex" direction="row" spacing={5} width="100%"></Stack>;
 }
 
--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -1,64 +1,9 @@
-import { useAuth } from '@/lib/auth';
-import Navbar from '@/components/admin/Navbar';
-import {
-     Box,
-     Button,
-     Center,
-     Grid,
-     GridItem,
-     useColorMode,
-     useColorModeValue
-} from '@chakra-ui/react';
-import Sidebar from '@/components/admin/Sidebar';
-import StickyBox from 'react-sticky-box';
 import Home from '@/components/admin/Home';
-import Head from 'next/head';
-import { Router, useRouter } from 'next/router';
-import { useEffect } from 'react';
+import DashboardLayout from '@/components/layout/DashboardLayout';
 
-const dashboard = ({ children }) => {
-     const { user, signout, loading } = useAuth();
-     const router = useRouter();
-     const bg = useColorModeValue('gray.100', 'gray.700');
-     const color = useColorModeValue('gray.100', 'gray.700');
-
-     return (
-          <Box>
-               <Head>
-                    <title>Athayog Dashboard</title>
-                    <script
-                         dangerouslySetInnerHTML={{
-                              __html: `
-                              if (!document.cookie && !document.cookie.includes('athayog-auth')) {
-                                   window.location.href = "/"
-                              }
-                         `
-                         }}
-                    />
-               </Head>
-               <Grid
-                    h="100%"
-                    templateRows="auto 1fr"
-                    templateColumns="repeat(10, 1fr)"
-               >
-                    <GridItem colSpan={1} rowSpan={3}>
-                         <StickyBox style={{ height: '100vh' }}>
-                              <Sidebar />
-                         </StickyBox>
-                    </GridItem>
-                    <GridItem colSpan={9} rowSpan={1} bg={bg}>
-                         <Navbar
-                              user={user}
-                              signout={signout}
-                              loading={loading}
-                         />
-                    </GridItem>
-                    <GridItem colSpan={9} rowSpan={2} bg={bg} px={8}>
-                         {children ? children : <Home />}
-                    </GridItem>
-               </Grid>
-          </Box>
-     );
+const dashboard = () => {
+     return <Home />;
 };
 
 export default dashboard;
+dashboard.Layout = DashboardLayout;
