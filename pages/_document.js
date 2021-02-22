4ae9118d61c9a716bb6bd2d56090a9080443d458
Harsimran <harsimransinghbarki@gmail.com>
2021-02-23 02:04:47 +0530

    possible font rendering fix

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,7 @@ class RootDocument extends Document {
                     <link
                          rel="stylesheet"
                          href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;300;400;500;600;700&display=swap"
+                         data-noprefix
                     />
                     <Head />
                     <body>
