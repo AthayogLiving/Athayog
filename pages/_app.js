de33bdd98ae47086f5320cca0ff6b7dc2c3c86ec
Harsimran <harsimransinghbarki@gmail.com>
2021-02-22 21:42:54 +0530

    added orignal n progess bar

--- pages/_app.js
+++ pages/_app.js
@@ -7,12 +7,32 @@ import { AuthProvider } from '@/lib/auth';
 import theme from '@/styles/theme.js';
 import { AnimatePresence } from 'framer-motion';
 import '@/styles/globals.css';
-
 import Head from 'next/head';
 import SEO from '../next-seo.config';
 import { DefaultSeo } from 'next-seo';
 import React from 'react';
 import { css, Global } from '@emotion/react';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+import { Router } from 'next/router';
+
+NProgress.configure({
+     showSpinner: false,
+     trickleRate: 0.1,
+     trickleSpeed: 300
+});
+
+Router.events.on('routeChangeStart', () => {
+     NProgress.start();
+});
+
+Router.events.on('routeChangeComplete', () => {
+     NProgress.done();
+});
+
+Router.events.on('routeChangeError', () => {
+     NProgress.done();
+});
 
 const GlobalStyle = ({ children }) => {
      return (
