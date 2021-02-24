abde4f9cae7ce95cb3786df1bcccd7d6568d0aad
Harsimran <harsimransinghbarki@gmail.com>
2021-02-24 16:34:12 +0530

    fixed home layout and dark mode

--- components/layout/HomeLayout.js
+++ components/layout/HomeLayout.js
@@ -1,16 +1,26 @@
-import { Button } from '@chakra-ui/react';
+import { Button, useColorMode } from '@chakra-ui/react';
 import Navbar from '../home/Navbar';
 import Footer from '../shared/Footer';
 import ScrollToTop from '../shared/ScrollToTop';
 import { v4 as uuidv4 } from 'uuid';
+import { useEffect } from 'react';
 
-const HomeLayout = ({ children }) => (
-     <div>
-          <Navbar />
-          {children}
-          <ScrollToTop key={uuidv4()} />
-          <Footer />
-     </div>
-);
+const HomeLayout = ({ children }) => {
+     const { colorMode, toggleColorMode } = useColorMode();
+
+     useEffect(() => {
+          if (colorMode === 'dark') {
+               toggleColorMode('light');
+          }
+     }, [colorMode]);
+     return (
+          <div>
+               <Navbar />
+               {children}
+               <ScrollToTop key={uuidv4()} />
+               <Footer />
+          </div>
+     );
+};
 
 export default HomeLayout;
