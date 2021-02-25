732dc1a025e2db6504df179164fee0137b715862
Harsimran <harsimransinghbarki@gmail.com>
2021-02-26 04:29:54 +0530

    added next pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const withImages = require('next-images');
 const path = require('path');
+const withPWA = require('next-pwa');
 
 module.exports = {
      sassOptions: {
@@ -11,4 +12,8 @@ module.exports = withImages({
      images: {
           domains: ['firebasestorage.googleapis.com']
      }
+});
+
+module.exports = withPWA({
+     // other next config
 });
