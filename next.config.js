6d76fcf65e033920144c9b76836c115de59206b7
Harsimran <harsimransinghbarki@gmail.com>
2021-02-27 18:27:56 +0530

    trying vercel redirect

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 const withImages = require('next-images');
 const path = require('path');
 const withPWA = require('next-pwa');
+const withMDX = require('@next/mdx')({
+     extension: /\.mdx?$/
+});
 
 module.exports = withPWA({
      // other next config
+});
+
+module.exports = withMDX({
+     pageExtensions: ['js', 'jsx', 'md', 'mdx']
 });
 
 module.exports = {
