93aba8630ea69b813d41a3f5f5f8a5c2becfa8da
Harsimran <harsimransinghbarki@gmail.com>
2021-03-10 03:34:17 +0530

    fixed admin

--- pages/api/admin/set-admin.js
+++ pages/api/admin/set-admin.js
@@ -22,48 +22,47 @@ export default async function handler(req, res) {
      const admin = req.body.admin;
      const id = req.body.id;
      if (req.method === 'POST') {
-          try {
-               auth.verifyIdToken(token).then((claims) => {
-                    if (
-                         typeof claims.email !== 'undefined' &&
-                         // typeof claims.email_verified !== 'undefined' &&
-                         // claims.email_verified &&
-                         claims.email.endsWith('@athayogliving.com')
-                    ) {
-                         // Add custom claims for additional privileges.
-                         auth.setCustomUserClaims(id, {
-                              admin: admin,
-                              role: role
-                         })
-                              .then(function (user) {
-                                   // Tell client to refresh token on user.
-                                   const data = {
-                                        admin,
-                                        metadata: {
-                                             role,
-                                             roleName: 'admin'
-                                        }
-                                   };
-                                   db.collection('adminUsers')
-                                        .doc(id)
-                                        .update(data);
-                              })
-                              .catch((error) => {
-                                   res.status(400).json({
-                                        message: 'Something happend try again'
+          auth.verifyIdToken(token).then((claims) => {
+               if (
+                    typeof claims.email !== 'undefined' &&
+                    // typeof claims.email_verified !== 'undefined' &&
+                    // claims.email_verified &&
+                    claims.email.endsWith('@athayogliving.com')
+               ) {
+                    // Add custom claims for additional privileges.
+                    console.log(id);
+                    auth.setCustomUserClaims(id, {
+                         admin: admin,
+                         role: role
+                    })
+                         .then(function (user) {
+                              // Tell client to refresh token on user.
+
+                              const data = {
+                                   admin,
+                                   metadata: {
+                                        role,
+                                        roleName: 'admin'
+                                   }
+                              };
+
+                              db.collection('adminUsers')
+                                   .doc(id)
+                                   .update(data)
+                                   .then((response) => {
+                                        return res.status(200).json({
+                                             message: 'Updated succesfull'
+                                        });
                                    });
-                              });
-                    } else {
-                         return res
-                              .status(422)
-                              .json({ message: 'Unverified email' });
-                    }
-               });
-               return res.status(200).json({
-                    message: 'Successfully given admin privilages'
-               });
-          } catch (error) {
-               return res.status(500).json(error);
-          }
+                         })
+                         .catch((error) => {
+                              return res.status(500).json(error);
+                         });
+               } else {
+                    return res
+                         .status(422)
+                         .json({ message: 'Unverified email' });
+               }
+          });
      }
 }
