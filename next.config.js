e2507839cf598e9a45943d6874fdd7504b09c9b2
Harsimran <harsimransinghbarki@gmail.com>
2021-03-10 23:20:29 +0530

    added runtime caching

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withPlugins = require('next-compose-plugins');
 const withImages = require('next-images');
 const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPlugins(
      [
