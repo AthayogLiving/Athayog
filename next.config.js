c69819ec3edc9a2e0a9f929569b8c97712dcf272
Athayog <admin@athayogliving.com>
2021-12-28 17:26:12 +0530

    updated next config

--- next.config.js
+++ next.config.js
@@ -30,9 +30,13 @@ module.exports = withPlugins(
                }
           ]
      ],
-     // {
-     //      experimental: { nftTracing: true }
-     // },
+     {
+          eslint: {
+               // Warning: This allows production builds to successfully complete even if
+               // your project has ESLint errors.
+               ignoreDuringBuilds: true
+          }
+     },
      {
           swcMinify: false
      }
