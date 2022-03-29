1ce37ca44c55179530a57668ae3bcac246f04280
Athayog <admin@athayogliving.com>
2022-03-29 22:50:45 +0530

    added facebook pixel

--- pages/_document.js
+++ pages/_document.js
@@ -46,28 +46,67 @@ class RootDocument extends Document {
 
                     <Head>
                          {/* Google Adsense */}
-                         <NextScript
-                              strategy="afterInteractive"
+                         <script
+                              data-ad-client="<Your value here>"
+                              async
+                              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+                         ></script>
+
+                         {/* Global Site Tag (gtag.js) - Google Analytics */}
+                         <script
+                              async
+                              src={`https://www.googletagmanager.com/gtag/js?id=AW-527303866`}
+                         />
+                         <script
                               dangerouslySetInnerHTML={{
                                    __html: `
-           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N4LH3M3');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-527303866', {
+              page_path: window.location.pathname,
+            });
           `
+                              }}
+                         />
+                         <script
+                              dangerouslySetInnerHTML={{
+                                   __html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+ fbq('init', '1913291218871066'); 
+fbq('track', 'PageView');
+          `
+                              }}
+                         />
+                         <script
+                              dangerouslySetInnerHTML={{
+                                   __html: `
+             gtag('event', 'conversion', {
+      'send_to': 'AW-527303866/3ewICPbt49ECELqJuPsB',
+      'transaction_id': ''
+  });
+          `
+                              }}
+                         />
+                         <script
+                              dangerouslySetInnerHTML={{
+                                   __html: `<noscript>
+ <img height="1" width="1" 
+src="https://www.facebook.com/tr?id=1913291218871066&ev=PageView
+&noscript=1"/>
+</noscript>`
                               }}
                          />
                     </Head>
                     <body>
                          <ColorModeScript
                               initialColorMode={theme.config.initialColorMode}
-                         />
-                         <noscript
-                              dangerouslySetInnerHTML={{
-                                   __html: `<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N4LH3M3"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`
-                              }}
                          />
                          <Main />
                          <NextScript />
