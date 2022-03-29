541f11af5f6b048673601c7c5fe7b645dbb7f969
Athayog <admin@athayogliving.com>
2022-03-29 22:03:15 +0530

    added gtag manager

--- pages/_document.js
+++ pages/_document.js
@@ -46,36 +46,15 @@ class RootDocument extends Document {
 
                     <Head>
                          {/* Google Adsense */}
-                         <script
-                              data-ad-client="<Your value here>"
-                              async
-                              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-                         ></script>
-
-                         {/* Global Site Tag (gtag.js) - Google Analytics */}
-                         <script
-                              async
-                              src={`https://www.googletagmanager.com/gtag/js?id=AW-527303866`}
-                         />
-                         <script
+                         <NextScript
+                              strategy="afterInteractive"
                               dangerouslySetInnerHTML={{
                                    __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-527303866', {
-              page_path: window.location.pathname,
-            });
-          `
-                              }}
-                         />
-                         <script
-                              dangerouslySetInnerHTML={{
-                                   __html: `
-             gtag('event', 'conversion', {
-      'send_to': 'AW-527303866/3ewICPbt49ECELqJuPsB',
-      'transaction_id': ''
-  });
+           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N4LH3M3');
           `
                               }}
                          />
@@ -83,6 +62,12 @@ class RootDocument extends Document {
                     <body>
                          <ColorModeScript
                               initialColorMode={theme.config.initialColorMode}
+                         />
+                         <noscript
+                              dangerouslySetInnerHTML={{
+                                   __html: `<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N4LH3M3"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`
+                              }}
                          />
                          <Main />
                          <NextScript />
