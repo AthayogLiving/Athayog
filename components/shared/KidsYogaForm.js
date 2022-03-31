9d0a5a291b2269cb76b85a7d9f27b26fe5e9fc36
Athayog <admin@athayogliving.com>
2022-03-31 15:22:10 +0530

    added base setup

--- components/shared/KidsYogaForm.js
+++ components/shared/KidsYogaForm.js
@@ -8,11 +8,20 @@ import {
      VStack,
      Button,
      HStack,
-     Stack
+     Stack,
+     Text,
+     Select,
+     Divider
 } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 function KidsYogaForm() {
+     const { handleSubmit, register, errors, reset } = useForm();
+
+     const handleData = (data) => {
+          console.log(data);
+     };
      return (
           <VStack
                bg="white"
@@ -21,36 +30,128 @@ function KidsYogaForm() {
                py={{ base: '5', md: '8' }}
                px={{ base: '5', md: '8' }}
                spacing={8}
+               as="form"
+               onSubmit={handleSubmit((data) => handleData(data))}
           >
+               <Text textAlign="left" width="100%" textTransform="uppercase">
+                    Parents Details
+               </Text>
                <Stack width="100%" direction={['column', 'row']} spacing="24px">
                     <FormControl>
-                         <FormLabel htmlFor="name">First Name</FormLabel>
-                         <Input id="name" type="name" />
+                         <FormLabel htmlFor="parentName">
+                              Parents Name
+                         </FormLabel>
+                         <Input
+                              id="parentName"
+                              type="text"
+                              name="parentName"
+                              ref={register({
+                                   required:
+                                        'Please enter your gaurdian/parent name'
+                              })}
+                         />
                     </FormControl>
                     <FormControl>
-                         <FormLabel htmlFor="name">Last Name</FormLabel>
-                         <Input id="name" type="name" />
+                         <FormLabel htmlFor="parentPhone">
+                              Phone number
+                         </FormLabel>
+                         <Input
+                              id="parentPhone"
+                              name="parentPhone"
+                              type="number"
+                              ref={register({
+                                   required: 'Please enter your parent phone'
+                              })}
+                         />
                     </FormControl>
                </Stack>
-
                <FormControl>
-                    <FormLabel htmlFor="email">Email address</FormLabel>
-                    <Input id="email" type="email" />
+                    <FormLabel htmlFor="parentEmail"> Email Address.</FormLabel>
+                    <Input
+                         id="parentEmail"
+                         type="email"
+                         name="email"
+                         ref={register({
+                              required: 'Please enter your email'
+                         })}
+                    />
                </FormControl>
 
+               <Divider />
+               <Text textAlign="left" width="100%" textTransform="uppercase">
+                    Kids Details
+               </Text>
+
+               <Stack width="100%" direction={['column', 'row']} spacing="24px">
+                    <FormControl>
+                         <FormLabel htmlFor="kidName">Name</FormLabel>
+                         <Input
+                              id="kidName"
+                              type="text"
+                              name="kidsName"
+                              ref={register({
+                                   required: 'Please enter kids name'
+                              })}
+                         />
+                    </FormControl>{' '}
+                    <FormControl>
+                         <FormLabel htmlFor="kidDOB">DOB</FormLabel>
+                         <Input
+                              id="kidDOB"
+                              type="date"
+                              name="date"
+                              ref={register({
+                                   required: 'Select a date'
+                              })}
+                         />
+                    </FormControl>
+               </Stack>
                <Stack width="100%" direction={['column', 'row']} spacing="24px">
                     {' '}
                     <FormControl>
-                         <FormLabel htmlFor="phone">Phone</FormLabel>
-                         <Input id="phone" type="phone" />
+                         <FormLabel htmlFor="published-date">Class</FormLabel>
+                         <Input
+                              id="kidClass"
+                              type="text"
+                              name="class"
+                              ref={register({
+                                   required: 'Please enter your class'
+                              })}
+                         />
                     </FormControl>
                     <FormControl>
-                         <FormLabel htmlFor="age">Age</FormLabel>
-                         <Input id="age" type="age" />
+                         <FormLabel htmlFor="published-date">
+                              School Name
+                         </FormLabel>
+                         <Input
+                              id="school"
+                              type="text"
+                              name="school"
+                              ref={register({
+                                   required: 'Please enter your school'
+                              })}
+                         />
                     </FormControl>
                </Stack>
                <FormControl>
-                    <Button colorScheme="green">Register</Button>
+                    <FormLabel htmlFor="published-date">Location</FormLabel>
+                    <Select
+                         name="location"
+                         ref={register({
+                              required: 'Please select a location'
+                         })}
+                    >
+                         <option value="online">
+                              Online (Indiranagar/KR Puram)
+                         </option>
+                         <option value="offline">Offline</option>
+                    </Select>
+               </FormControl>
+
+               <FormControl>
+                    <Button type="submit" colorScheme="green">
+                         Register
+                    </Button>
                </FormControl>
           </VStack>
      );
--- pages/kids-yoga-camp.js
+++ pages/kids-yoga-camp.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomeLayout from '@/components/layout/HomeLayout';
 import {
+     AspectRatio,
      Box,
      Button,
      Flex,
@@ -13,13 +14,17 @@ import {
      Stack,
      StackDivider,
      Text,
-     UnorderedList
+     UnorderedList,
+     chakra,
+     VStack
 } from '@chakra-ui/react';
 import NavbarHelper from '@/components/shared/NavbarHelper';
 import { MdCheckCircle } from 'react-icons/md';
 import { FaDotCircle } from 'react-icons/fa';
 import { BsArrowRightCircleFill } from 'react-icons/bs';
 import KidsYogaForm from '@/components/shared/KidsYogaForm';
+import summerOne from 'public/summer_1.jpg';
+import Image from 'next/image';
 
 function KidsYogaCamp() {
      return (
@@ -29,44 +34,41 @@ function KidsYogaCamp() {
                     height="60vh"
                     justifyContent="center"
                     alignItems="center"
-                    bgImage="linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)),url('https://images.unsplash.com/photo-1543858828-7cf1a9beb95c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')"
+                    bgImage="linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.6)),url('https://images.unsplash.com/photo-1543858828-7cf1a9beb95c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')"
                     bgSize="cover"
                     bgPosition="center"
                     direction="column"
+                    position="relative"
                >
-                    <Heading fontWeight="bold" fontSize="4xl" textColor="white">
-                         Kids Yoga Camp
+                    <Heading
+                         fontSize="5xl"
+                         fontWeight="normal"
+                         textColor="white"
+                         textAlign="center"
+                    >
+                         Unlock Your Little Ones Potential <br />
+                         At AYL&#39;S{' '}
+                         <chakra.span
+                              textColor="aygreen.300"
+                              borderBottom="3px solid"
+                              fontWeight="bold"
+                              cursor="pointer"
+                         >
+                              Kids Yoga Camp
+                         </chakra.span>
                     </Heading>
-                    <Button mt="3" colorScheme="green">
-                         Register Now
-                    </Button>
-               </Flex>
-
-               <Box
-                    maxW="container.lg"
-                    margin="0 auto"
-                    py={{ base: '5', md: '10' }}
-                    px="5"
-               >
-                    <Text maxW="container.lg" margin="0 auto">
-                         Lorem ipsum dolor sit amet consectetur adipisicing
-                         elit. Vel eos dicta ullam obcaecati. Ut provident
-                         doloribus dolorem sint corrupti quas esse quae, id
-                         iure, architecto sequi laudantium necessitatibus saepe
-                         accusantium eum unde quisquam est. Unde similique, et,
-                         labore, excepturi dignissimos quas necessitatibus
-                         voluptatibus aliquam repudiandae provident error magnam
-                         aperiam adipisci?
-                    </Text>
                     <Stack
                          border="1.5px solid"
                          borderColor="gray.100"
                          py="3"
                          boxShadow="base"
+                         bg="white"
                          px="5"
                          rounded="xl"
                          spacing={{ base: '5', md: '8' }}
-                         width="100%"
+                         width="container.lg"
+                         position="absolute"
+                         bottom="-20px"
                          divider={<StackDivider borderColor="gray.300" />}
                          mt="8"
                          align={{ base: 'baseline', md: 'center' }}
@@ -76,31 +78,96 @@ function KidsYogaCamp() {
                          }}
                          direction={{ base: 'column', md: 'row' }}
                     >
-                         <Flex direction="column">
-                              <Box fontWeight="bold">Age</Box>{' '}
-                              <Box textColor="gray.600">6 - 11yr</Box>
+                         <Flex direction="column" alignItems="center">
+                              <Button colorScheme="orange">Register Now</Button>
                          </Flex>
                          <Flex direction="column">
-                              <Box fontWeight="bold">Date</Box>{' '}
-                              <Box textColor="gray.600">2nd May - 13th May</Box>
+                              <Box textColor="gray.600">Age</Box>{' '}
+                              <Box textColor="gray.900" fontWeight="medium">
+                                   6 - 11yr
+                              </Box>
                          </Flex>
                          <Flex direction="column">
-                              <Box fontWeight="bold">Time</Box>{' '}
-                              <Box textColor="gray.600">11am - 1pm </Box>
+                              <Box textColor="gray.600">Date</Box>{' '}
+                              <Box textColor="gray.900" fontWeight="medium">
+                                   2nd May - 13th May
+                              </Box>
                          </Flex>
                          <Flex direction="column">
-                              <Box fontWeight="bold">Location</Box>
-                              <Box textColor="gray.600">
+                              <Box textColor="gray.600">Time</Box>{' '}
+                              <Box textColor="gray.900" fontWeight="medium">
+                                   11am - 1pm{' '}
+                              </Box>
+                         </Flex>
+                         <Flex direction="column">
+                              <Box textColor="gray.600">Location</Box>
+                              <Box textColor="gray.900" fontWeight="medium">
                                    Indiranagar / KR Puram / Online
                               </Box>
                          </Flex>
-                    </Stack>
+                    </Stack>{' '}
+               </Flex>
+
+               <Box
+                    maxW="80vw"
+                    margin="0 auto"
+                    py={{ base: '5', md: '10' }}
+                    px="5"
+                    mt={8}
+               >
+                    <Text margin="0 auto" fontSize="xl">
+                         It’s that time of the year when the days are long and
+                         hot! AthaYog’s Summer Kid’s Camp to give you and your
+                         kids’ a break!
+                    </Text>
+
+                    <Text margin="0 auto" fontSize="xl" mt={8}>
+                         This summer, let your kids chill out in their own zone
+                         engaged in fun activities and learning.
+                    </Text>
+                    <List spacing={3} mt={5} fontSize="xl">
+                         <ListItem>
+                              <ListIcon
+                                   as={BsArrowRightCircleFill}
+                                   color="aygreen.500"
+                              />
+                              Move, Pose & Breathe in the Yoga Asana Class
+                         </ListItem>
+                         <ListItem>
+                              <ListIcon
+                                   as={BsArrowRightCircleFill}
+                                   color="aygreen.500"
+                              />
+                              Chant your Hearts out at Satsang
+                         </ListItem>
+                         <ListItem>
+                              <ListIcon
+                                   as={BsArrowRightCircleFill}
+                                   color="aygreen.500"
+                              />
+                              Learn through Play with Yoga games & more
+                         </ListItem>
+                         <ListItem>
+                              <ListIcon
+                                   as={BsArrowRightCircleFill}
+                                   color="aygreen.500"
+                              />
+                              Group activities to Nurture the Social skills
+                         </ListItem>
+                         <ListItem>
+                              <ListIcon
+                                   as={BsArrowRightCircleFill}
+                                   color="aygreen.500"
+                              />
+                              Sattvic snacks to Build Healthy Eating Habits
+                         </ListItem>
+                    </List>
 
                     <Box mt={16}>
                          <Heading textTransform="uppercase" fontSize="2xl">
                               Classes includes{' '}
                          </Heading>
-                         <List mt={3} spacing={2}>
+                         <List mt={3} spacing={2} fontSize="xl">
                               <ListItem>
                                    {' '}
                                    <ListIcon
@@ -143,6 +210,11 @@ function KidsYogaCamp() {
                               </ListItem>
                          </List>
                     </Box>
+               </Box>
+               <Box maxW="80vw" margin="0 auto" py={{ base: '5', md: '10' }}>
+                    <Heading>
+                         WHY SHOULD I ENROLL MY KIDS FOR THE SUMMER KID’S CAMP?
+                    </Heading>
                </Box>
                <Box bg="gray.50">
                     <Box
