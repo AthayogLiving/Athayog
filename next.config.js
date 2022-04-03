14bdafe5b2c951c5151dbcfa88ac6a901adede32
Athayog <admin@athayogliving.com>
2022-04-03 17:38:10 +0530

    disable eslint for prod

--- next.config.js
+++ next.config.js
@@ -26,14 +26,15 @@ module.exports = withPlugins(
                ]
           }
      },
+
+     {
+          swcMinify: true
+     },
      {
           eslint: {
                // Warning: This allows production builds to successfully complete even if
                // your project has ESLint errors.
                ignoreDuringBuilds: true
           }
-     },
-     {
-          swcMinify: true
      }
 );
